619563d38b2d21c4a0df0a8ca756ca171e4cb6e7
ElinaXproject <aicompan1p@gmail.com>
2026-08-12 06:19:15 +0200

    Refactor main.js to include add and view components

--- src/windows/ui/main.js
+++ src/windows/ui/main.js
@@ -1,7 +1,50 @@
+const crud = require('../db/crud');
 const header = require('./components/header');
-const list = require('./components/list');
+const add = require('./components/add');
+const view = require('./components/view');
 
-window.onload = () => {
+function renderList() {
+    const entries = crud.getAll();
+    let html = "<h2>Twoje wpisy</h2>";
+
+    if (entries.length === 0) {
+        html += "<p>Brak wpisów.</p>";
+    } else {
+        entries.forEach(e => {
+            html += `<div class="item" data-id="${e.id}">
+                        <b>${e.title}</b><br>
+                        ${e.username}
+                     </div>`;
+        });
+    }
+
+    document.getElementById('list').innerHTML = html;
+
+    document.querySelectorAll('.item').forEach(el => {
+        el.onclick = () => {
+            const id = el.getAttribute('data-id');
+            const entry = crud.get(id);
+            document.getElementById('app').innerHTML = view.render(entry);
+            view.init(id);
+        };
+    });
+}
+
+function render() {
     const app = document.getElementById('app');
-    app.innerHTML = header.render() + list.renderEmpty();
-};
+
+    app.innerHTML = `
+        ${header.render()}
+        <button id="addBtn">Dodaj wpis</button>
+        <div id="list"></div>
+    `;
+
+    document.getElementById('addBtn').onclick = () => {
+        app.innerHTML = add.render();
+        add.init();
+    };
+
+    renderList();
+}
+
+window.onload = render;
