9d9734a57c764b95ef2f51bac8e3fbfa93fbfe8e
ElinaXproject <aicompan1p@gmail.com>
2026-08-12 05:55:18 +0200

    Refactor updater.js to remove loadVersion
    Removed loadVersion function and added check method.

--- src/shared/updater.js
+++ src/shared/updater.js
@@ -1,15 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-
 module.exports = {
-    loadVersion() {
-        try {
-            const file = path.join(__dirname, '../../version.json');
-            const data = fs.readFileSync(file, 'utf8');
-            return JSON.parse(data);
-        } catch (err) {
-            console.error("Błąd ładowania version.json:", err);
-            return null;
-        }
-    }
+    check() { return "OK"; }
 };
